830aa6ceb90ece810116f64edbdee1f7838bc3b6
RIZQILLAH <kazuryokun@gmail.com>
2022-09-23 22:16:29 +0700

    New

--- index.php
+++ index.php
@@ -2,16 +2,3 @@
 $data = file_get_contents("data.json");
 
 print_r($data);
-
-
-?>
-<!-- <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-<script>
-  $.getJSON("data.json", function(json) {
-
-    for (let i = 0; i < Object.keys(json).length; i++) {
-      document.write("Nama : " + json[i]["nama"] + "<br>");
-      document.write("NIM : " + json[i]["NIM"] + "<br><br>");
-    }
-  });
-</script> -->
