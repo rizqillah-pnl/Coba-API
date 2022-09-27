b87b0f83cb791b61c990d6dbd3e8bf3b4550996c
RIZQILLAH <kazuryokun@gmail.com>
2022-09-27 12:05:53 +0700

    New Searching

--- index.php
+++ index.php
@@ -2,4 +2,38 @@
 $data = file_get_contents("data.json");
 
 header('Content-Type: application/json; charset=utf-8');
-echo $data;
+
+
+if (isset($_GET['nama']) || isset($_GET['nim'])) {
+  if (isset($_GET['nama'])) {
+    $nama = $_GET['nama'];
+  } else if (isset($_GET['nim'])) {
+    $nim = $_GET['nim'];
+  }
+
+  $data = json_decode($data);
+
+  $res = null;
+  for ($i = 0; $i < count($data); $i++) {
+    if (isset($_GET['nama'])) {
+      if (strtolower($data[$i]->nama) == strtolower($nama)) {
+        $res = $i;
+        break;
+      }
+    } else if (isset($_GET['nim'])) {
+
+      if ($data[$i]->NIM == $nim) {
+        $res = $i;
+        break;
+      }
+    }
+  }
+
+  if ($res != null) {
+    print_r(json_encode($data[$res]));
+  } else {
+    echo "Data Tidak Ditemukan!";
+  }
+} else {
+  echo $data;
+}
