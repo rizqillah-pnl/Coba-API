e9d394e30827945612ce6cb8e0e9251678d2257c
RIZQILLAH <kazuryokun@gmail.com>
2022-09-29 11:40:14 +0700

    New JSON

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
+// ambil data json
 $data = file_get_contents("data.json");
 
+// Ubah tipe konten ke json
 header('Content-Type: application/json; charset=utf-8');
 
-
+// Cek jika ada parameter GET dari nama atau nim
 if (isset($_GET['nama']) || isset($_GET['nim'])) {
+  // Cek jika ada parameter nama, jika tidak cek parameter nim
   if (isset($_GET['nama'])) {
     $nama = $_GET['nama'];
   } else if (isset($_GET['nim'])) {
     $nim = $_GET['nim'];
   }
 
+  // Ubah data json menjadi array
   $data = json_decode($data);
 
+  // Melakukan perulangan untuk mendapat index dari data yang dicari
   $res = null;
   for ($i = 0; $i < count($data); $i++) {
     if (isset($_GET['nama'])) {
@@ -22,6 +27,7 @@ if (isset($_GET['nama']) || isset($_GET['nim'])) {
       }
     } else if (isset($_GET['nim'])) {
 
+      // Jika data ditemukan, keluar dari looping
       if ($data[$i]->NIM == $nim) {
         $res = $i;
         break;
@@ -29,14 +35,18 @@ if (isset($_GET['nama']) || isset($_GET['nim'])) {
     }
   }
 
+  // jika res tidak null atau data ditemukan
+  // tampilkan data dari objek dengan bentuk json ke layar
   if ($res != null) {
     $data[$res]->status = "200 OK";
     print_r(json_encode($data[$res]));
   } else {
+    // jika data yang dicari tidak ada tampilkan error
     $data = json_encode(array('status' => '404 Error', 'detail' => 'Data Tidak Ditemukan!'));
 
     print_r($data);
   }
 } else {
+  // jika tidak ada parameter, tampilkan semua data
   print_r($data);
 }
